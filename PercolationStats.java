7f7e5749bdab5f3fc0925b4dbf810d87afdd8b96
raios168 <raios168@gmail.com>
2020-04-22 18:29:18 -0400

    finished stats but not working yet

--- PercolationStats.java
+++ PercolationStats.java
@@ -4,19 +4,23 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
+    private int numOfTrials;
     private int threshold;
     private int[] trialResults;
     private double mean;
+    private double stddev;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         // array to hold results of all the trials
         trialResults = new int[trials];
+        numOfTrials = trials;
 
         for (int i = 0; i < trials; i++) {
             // after every trial, we create a new percolation object
@@ -42,22 +46,35 @@ public class PercolationStats {
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 1;
+        stddev = StdStats.stddev(trialResults);
+        return stddev;
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 1;
+        double confidenceLo = mean - ((1.96 * stddev) / Math.sqrt(numOfTrials));
+        return confidenceLo;
     }
 
     public double confidenceHi() {
-        return 1;
+        double confidenceHi = mean + ((1.96 * stddev) / Math.sqrt(numOfTrials));
+        return confidenceHi;
     }
 
     // test client
     public static void main(String[] args) {
-        for (int i = 0; i <= 50; i++) {
-            System.out.println(StdRandom.uniform(1, 11));
-        }
+        int sizeOfGrid = StdIn.readInt();
+        int numOfTrials = StdIn.readInt();
+
+        PercolationStats perc = new PercolationStats(sizeOfGrid, numOfTrials);
+        double myMean = perc.mean();
+        double myStddev = perc.stddev();
+        double myConfidenceHi = perc.confidenceHi();
+        double myConfidenceLo = perc.confidenceLo();
+
+        System.out.println("mean = " + myMean);
+        System.out.println("stddev = " + myStddev);
+        System.out.println("95% confidence interval = [" + myConfidenceLo + ", "
+                                   + myConfidenceHi + "]");
     }
 }
